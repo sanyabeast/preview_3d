3839181f6c239c1d298b3b83979ae563b17cc2fd
sanyabeast <a.gvrnsk@gmail.com>
2023-01-27 04:47:25 +0200

    basic setup

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-404: Not Found
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let file_to_open = path.resolve(process.cwd(), process.argv[2]);
+console.log(file_to_open)
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1600,
+        height: 900,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            devTools: true,
+            contextIsolation: false,
+            nodeIntegration: true,
+            additionalArguments: [
+                '[*]',
+                file_to_open
+            ]
+        }
+    })
+    mainWindow.loadFile(`index.html`)
+    mainWindow.webContents.openDevTools()
+}
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
---
+++ preload.js
@@ -0,0 +1,15 @@
+const path = require('path')
+let node_tools = {
+    path: path
+}
+
+if (process.argv[process.argv.length - 2] === '[*]') {
+    window.file_to_open = process.argv[process.argv.length - 1]
+
+    console.log(`file to open: ${window.file_to_open}`)
+}
+
+console.log(process.argv)
+
+console.log(window, node_tools)
+window.node_tools = node_tools
