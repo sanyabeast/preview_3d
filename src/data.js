0731bb7d54cdd9e15a5c0c3fa8ae985625d11a48
sanyabeast <a.gvrnsk@gmail.com>
2023-02-02 20:35:30 +0200

    data: releases url updated

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 
 /** Created by @sanyabeast | 28 Jan 2023 | Kyiv, Ukraine */
 
-const release_page_url = 'https://github.com/sanyabeast/preview_3d/releases'
+const release_page_url = 'https://github.com/sanyabeast/preview_3d/releases/tag/stable'
 const update_check_url = 'https://raw.githubusercontent.com/sanyabeast/preview_3d/main/package.json'
 
 export {
